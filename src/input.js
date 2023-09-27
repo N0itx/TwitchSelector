f68bcc7cfd627a72c79d279e7a9e57723251da2e
ShiroLB <45373591+N0itx@users.noreply.github.com>
2023-09-27 00:15:15 -0600

    Add files via upload

--- src/input.js
+++ src/input.js
@@ -1,7 +1,7 @@
 const input = document.getElementById('broadcaster');
-const palabras = ['SoyPan', 'elded', 'JuanSGuarnizo', 'ElMariana', 'AldoGeo', 'BarcaGamer', 'NataliaMX'];
+const palabras = ['SoyPollus', 'eldick', 'JhonGranizo', 'LaMarina', 'Cokas', 'BarcoRGB', 'Natrolia'];
 let palabraActual = 0; // Índice de la palabra actual
-let letraActual = 0;  // Índice de la letra actual
+let letraActual = 0; // Índice de la letra actual
 
 function escribirLetra() {
   if (palabraActual < palabras.length) {
@@ -12,13 +12,13 @@ function escribirLetra() {
       letraActual++;
       setTimeout(escribirLetra, 50); // 100 milisegundos de retraso entre letras
     } else {
-      setTimeout(borrarPalabra, 1000); // 1 segundo de retraso antes de borrar la palabra
+      setTimeout(borrarPalabra, 500); // 1 segundo de retraso antes de borrar la palabra
     }
   } else {
     // Una vez que se completan todas las palabras, reinicia el proceso
     palabraActual = 0;
     letraActual = 0;
-    setTimeout(escribirLetra, 1000); // 1 segundo de retraso antes de comenzar de nuevo
+    setTimeout(escribirLetra, 500); // 1 segundo de retraso antes de comenzar de nuevo
   }
 }
 
@@ -32,7 +32,7 @@ function borrarPalabra() {
   } else {
     palabraActual++;
     letraActual = 0;
-    setTimeout(escribirLetra, 1000); // 1 segundo de retraso antes de escribir la siguiente palabra
+    setTimeout(escribirLetra, 500); // 1 segundo de retraso antes de escribir la siguiente palabra
   }
 }
 
